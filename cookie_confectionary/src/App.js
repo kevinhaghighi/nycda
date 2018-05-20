00af1d6b529e6082be4998b36633277d52280c2c
Kevin <kevinhaghighi@gmail.com>
2018-05-19 21:49:18 -0400

    cookie

--- cookie_confectionary/src/App.js
+++ cookie_confectionary/src/App.js
@@ -1,56 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Counter from './Counter';
-var Cookies = require('cookies-js');
-let initialSugarValue = Cookies.get('sugar_cookie');
-let initialLemonValue = Cookies.get('lemon_cookie');
-let initialChocolateValue = Cookies.get('chocolate_cookie');
+import Cookies from 'cookies-js';
+
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      chocolate_count: initialChocolateValue,
-      lemon_count: initialLemonValue,
-      sugar_count: initialSugarValue
+      chocolateCount: this.getCount('chocolate'),
+      lemonCount: this.getCount('lemon'),
+      sugarCount: this.getCount('sugar')
     };
-    this.sugarCookieCreator = this.sugarCookieCreator.bind(this);
-    this.lemonCookieCreator = this.lemonCookieCreator.bind(this);
-    this.chocolateCookieCreator = this.chocolateCookieCreator.bind(this);
-    this.clearCookies = this.clearCookies.bind(this)
+    this.setSugarCookie = this.setSugarCookie.bind(this);
+    this.setChocolateCookie = this.setChocolateCookie.bind(this);
+    this.setLemonCookie = this.setLemonCookie.bind(this);
+    this.clearCookies = this.clearCookies.bind(this);
   }
-
-  sugarCookieCreator(e){  
-    let newCount = parseInt(this.state.sugar_count, 10) + 1
-    Cookies.set('sugar_cookie', `${newCount}`);
+    
+  getCount(cookie) {
+    return parseInt(Cookies.get(cookie)) || 0;
+  }
+    
+  setSugarCookie() {
+    const currentCount = this.getCount('sugar');
+    Cookies.set('sugar', currentCount + 1);
     this.setState({
-      sugar_count: Cookies.get('sugar_cookie')
+      sugarCount: currentCount + 1
     });
   }
-  lemonCookieCreator(e){  
-    let newCount = parseInt(this.state.lemon_count, 10) + 1
-    Cookies.set('lemon_cookie', `${newCount}`);
+    
+  setChocolateCookie() {
+    const currentCount = this.getCount('chocolate');
+    Cookies.set('chocolate', currentCount + 1);
     this.setState({
-      lemon_count: Cookies.get('lemon_cookie')
+      chocolateCount: currentCount + 1
     });
   }
-  chocolateCookieCreator(e){  
-    let newCount = parseInt(this.state.chocolate_count, 10) + 1
-    Cookies.set('chocolate_cookie', `${newCount}`);
+    
+  setLemonCookie() {
+    let currentCount = this.getCount('lemon');
+    Cookies.set('lemon', currentCount + 1);
     this.setState({
-      chocolate_count: Cookies.get('chocolate_cookie')
-    });
+        lemonCount: currentCount + 1
+      });
   }
-
-  clearCookies(e) { 
-    Cookies.set('chocolate_cookie', '0');
-    Cookies.set('sugar_cookie', '0');
-    Cookies.set('lemon_cookie', '0');
+    
+  clearCookies() {
+    function expireCookie (cookie) {
+      Cookies.expire(cookie);
+    }
+    expireCookie('sugar');
+    expireCookie('chocolate');
+    expireCookie('lemon');
     this.setState({
-      lemon_count: Cookies.get('lemon_cookie'),
-      sugar_count: Cookies.get('lemon_cookie'),
-      chocolate_count: Cookies.get('lemon_cookie')
+      chocolateCount: 0,
+      sugarCount: 0,
+      lemonCount: 0
     })
   }
 
@@ -60,20 +66,20 @@ class App extends Component {
         <h1>WHAT WOULD YOU LIKE TO EAT?</h1>
         <h2>Click on a cookie below</h2>
         <div className="cookie-container">
-            <div className="cookie sugar" onClick={this.sugarCookieCreator}>
+            <div id="surgaCount" className="cookie sugar" onClick={this.setSugarCookie}>
                 <h4>SUGAR COOKIES</h4>
                 <img src="https://sweetpeaskitchen.files.wordpress.com/2010/09/dsc_1583.jpg" alt="sugar cookie"></img>
-                <p>Cookies eaten: {this.state.sugar_count}</p>
+                <p>Cookies eaten: {this.state.sugarCount}</p>
             </div>
-            <div className="cookie chocolate" onClick={this.chocolateCookieCreator}>
+            <div id="chocolateCount" className="cookie chocolate" onClick={this.setChocolateCookie}>
                 <h4>CHOCOLATE COOKIES</h4>
                 <img src="http://www.bakenquilt.com/Wordpress/wp-content/uploads/2012/05/momofuku_chocolate.jpg" alt="chocolate cookie"></img>
-                <p>Cookies eaten: {this.state.chocolate_count}</p>
+                <p>Cookies eaten: {this.state.chocolateCount}</p>
             </div>
-            <div className="cookie lemon" onClick={this.lemonCookieCreator}>
+            <div id="lemonCount" className="cookie lemon" onClick={this.setLemonCookie}>
                 <h4>LEMON COOKIES</h4>
                 <img src="https://www.modernhoney.com/wp-content/uploads/2016/03/DSC_0212-copy.jpg" alt="lemon cookie"></img>
-                <p>Cookies eaten: {this.state.lemon_count}</p>
+                <p>Cookies eaten: {this.state.lemonCount}</p>
             </div>
         </div>
         <button className="clear-cookies-button" onClick={this.clearCookies}>Clear your cookies here</button>
